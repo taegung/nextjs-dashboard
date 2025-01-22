c3e1f199f980e5ce427e576e62edc7523b66542a
taegung <yesongmin12@gamil.com>
2025-01-22 14:19:08 +0900

    정적 및 동적 렌더링

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -15,12 +15,12 @@ export async function fetchRevenue() {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
+    console.log('Data fetch completed after 3 seconds.');
 
     return data.rows;
   } catch (error) {
@@ -65,7 +65,7 @@ export async function fetchCardData() {
       invoiceCountPromise,
       customerCountPromise,
       invoiceStatusPromise,
-    ]);
+    ]);// 병렬 데이터 페칭
     console.log(data[0]);
 
     const numberOfInvoices = Number(data[0].rows[0].count ?? '0');
